862fd4a623545393b52029e6f6a7c26d414528a8
xuyucai <xuyucai@meideng.net>
2023-01-15 13:06:34 +0000

    impl insert

--- src/lib.rs
+++ src/lib.rs
@@ -22,52 +22,121 @@ pub mod utils {
     }
 }
 
+pub mod traits {
+    pub trait Executable {
+        fn execute(&self);
+    }
+}
+
+pub mod entity {
+    use crate::types::Entity;
+    use std::{fmt};
+
+    impl TryFrom<Vec<String>> for Entity {
+        type Error = &'static str;
+
+        fn try_from(words: Vec<String>) -> Result<Self, Self::Error> {
+            let id = match words.get(0) {
+                Some(s) => match s.parse::<u32>() {
+                    Ok(v) => v,
+                    Err(_) => return Err("id must be a number"),
+                },
+                None => return Err("id is required"),
+            };
+            let username = match words.get(1) {
+                Some(s) => s,
+                None => return Err("username is required"),
+            };
+            let email = match words.get(2) {
+                Some(s) => s,
+                None => return Err("email is required"),
+            };
+            Ok(Entity {
+                id,
+                username: username.to_string(),
+                email: email.to_string(),
+            })
+        }
+    }
+
+    // impl Into<Vec<u8>> for Entity {
+    //     fn into(self) -> Vec<u8> {
+    //         let mut bytes = Vec::new();
+    //         bytes.extend(self.id.to_be_bytes().iter());
+    //         bytes.extend(self.username.as_bytes());
+    //         bytes.extend(self.email.as_bytes());
+    //         bytes.as_slice()
+    //     }
+    // }
+
+    impl fmt::Display for Entity {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "({}, {}, {})", self.id, self.username, self.email)
+        }
+    }
+}
+
 pub mod types {
+
     pub enum MetaCommandResult {
         MetaCommandSuccess,
         MetaCommandUnrecognizedCommand,
     }
 
-    pub enum StatementType {
-        StatementInsert,
-        StatementSelect,
+    #[derive(Debug)]
+    pub struct Entity {
+        pub id: u32,
+        pub username: String,
+        pub email: String,
     }
 
-    pub struct Statement {
-        pub statement_type: StatementType,
+    pub enum Statement {
+        StatementInsert(Entity),
+        StatementSelect,
     }
 }
 
 pub mod statement {
-    use crate::types;
 
-    use crate::types::StatementType;
+    use crate::types::{Entity, Statement};
 
-    use crate::types::Statement;
+    pub fn prepare_statement(raw_input: &str) -> Result<Statement, &str> {
+        let words: Vec<String> = raw_input
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+        let first = words.get(0);
+        let tag = match first {
+            Some(s) => s,
+            None => return Err("Empty input, please input something"),
+        };
 
-    pub fn excute_statement(statement: &Statement) {
-        match statement.statement_type {
-            StatementType::StatementInsert => println!("This is where we would do an insert."),
-            StatementType::StatementSelect => println!("This is where we would do a select."),
+        match tag.as_str() {
+            super::constants::statement::tag::INSERT => {
+                let data_words = words[1..].to_vec();
+                let entity = match Entity::try_from(data_words) {
+                    Ok(v) => v,
+                    Err(e) => return Err(e),
+                };
+                Ok(Statement::StatementInsert(entity))
+            }
+            super::constants::statement::tag::SELECT => Ok(Statement::StatementSelect),
+            _ => Err("Unrecognized tag"),
         }
     }
 
-    pub fn prepare_statement(raw_input: &str) -> Option<StatementType> {
-        let tag = match raw_input.split_whitespace().next() {
-            Some(s) => s,
-            None => return None,
-        };
-        match tag {
-            super::constants::statement::tag::INSERT => Some(types::StatementType::StatementInsert),
-            super::constants::statement::tag::SELECT => Some(types::StatementType::StatementSelect),
-            _ => None,
+    impl crate::traits::Executable for Statement {
+        fn execute(&self) {
+            match self {
+                Statement::StatementInsert(v) => println!("got {}", v),
+                Statement::StatementSelect => println!("This is where we would do a select."),
+            }
         }
     }
 }
 
 pub mod interact {
-    use crate::statement;
-    use crate::types::{Statement};
+    use crate::{statement, traits::Executable};
 
     pub fn print_prompt() {
         print!("{}", super::constants::PROMPT);
@@ -78,7 +147,7 @@ pub mod interact {
     pub fn do_meta_command(command: &str) -> super::types::MetaCommandResult {
         match command {
             super::constants::command::EXIT => {
-                 on_exit();
+                on_exit();
                 std::process::exit(0);
             }
             _ => return super::types::MetaCommandResult::MetaCommandUnrecognizedCommand,
@@ -102,14 +171,13 @@ pub mod interact {
                 }
             }
             let statement = match statement::prepare_statement(raw_input) {
-                Some(v) => Statement { statement_type: v },
-                None => {
-                    println!("Unrecognized keyword at start of '{}'", raw_input);
+                Ok(v) => v,
+                Err(e) => {
+                    println!("Parse failed: {}", e);
                     continue;
                 }
             };
-            statement::excute_statement(&statement);
+            statement.execute();
         }
     }
-
 }
